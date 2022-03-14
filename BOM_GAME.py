27232bea4e317e915162a3c9452a8cd435155ebf
Jacob Bastian <jcubby86@hotmail.com>
2022-03-14 12:53:39 -0600

    Added start and play again states

--- BOM_GAME.py
+++ BOM_GAME.py
@@ -7,35 +7,35 @@ HEIGHT = 600
 def getImg(direction, offset1, offset2, offset3):
     return folder + direction + img + str(offset1 + offset2 + offset3)
 
+
 def draw():
-    global timer
-    if timer > 500:
-        return
     screen.clear()
     grass.draw()
+    
+    if gameState == 'start':
+        screen.draw.text("PRESS SPACE TO START", center=(WIDTH/2, HEIGHT/4), fontsize = 60)
+        return
+
     sortActors()
     for a in actors:
         if sRect.colliderect(a):
             a.draw()
 
     if gameState == 'defend':
-        timer += 1
         screen.draw.text("DEFEND THE FLOCKS!", center=(WIDTH/2, HEIGHT/4), fontsize = 72)
-    if gameState == 'win':
-        timer += 1
+    elif gameState == 'win':
         screen.draw.text("YOU DEFENDED THE KING'S FLOCKS!!!", center=(WIDTH/2, HEIGHT/4), fontsize = 60)
-    if ammon.isDead:
-        timer += 2
+    elif gameState == 'lose':
         screen.draw.text("THE LAMANITES SCATTERED THE SHEEP", center=(WIDTH/2, HEIGHT/4), fontsize = 60)
-
+    elif gameState == 'again':
+        screen.draw.text("PRESS SPACE TO PLAY AGAIN", center=(WIDTH/2, HEIGHT/4), fontsize = 60)
 
 
 def update():
     global sheepDir, enemiesKilled
-    if timer > 500:
-        return
-    updateSprite(ammon)
+    if gameState == 'again' or gameState == 'start': return
     if gameState == 'sheep-off':
+        updateSprite(ammon)
         moveSheep(3)
         if ammon.x > WIDTH/2:
             ammon.x -= ammonSpeed
@@ -44,6 +44,7 @@ def update():
         return
     if gameState == 'main' and enemiesKilled == enemyCount:
         updateState('win')
+        clock.schedule_unique(againState, 5)
         sheepDir = "left"
         for s in sheep:
             s.bottom = random.randint(s.height, HEIGHT)
@@ -51,6 +52,7 @@ def update():
         moveSheep(-2)
 
     playerMovement()
+    updateSprite(ammon)
 
     for arm in arms:
         if arm.bottom < arm.stop:
@@ -73,8 +75,11 @@ def update():
             enemy.isDead = True
             enemy.dir = "left"
             enemiesKilled += 1
-        if enemy.isAttacking and enemy.attacking//slowAttack > 1 and checkattack(enemy, ammon):
+        if gameState == 'main' and enemy.isAttacking and enemy.attacking//slowAttack > 1 and checkattack(enemy, ammon):
             ammon.isDead = True
+            updateState('lose')
+            clock.schedule_unique(againState, 3)
+
 
 def enemyMovement(enemy):
     enemy.isMoving = False
@@ -93,9 +98,10 @@ def enemyMovement(enemy):
         if enemy.bottom <= ammon.y + attackAdj:
             enemy.isMoving = True
             enemy.y += enemySpeed
-        if not enemy.isMoving and not ammon.isDead:
+        if not enemy.isMoving and gameState == 'main':
             enemy.isAttacking = True
             enemy.attacking = random.randint(-3 * slowAttack, 0)
+
 
 def makeArm(enemy):
     arm = Actor(folder + enemy.dir + "/arm", (enemy.x, enemy.y))
@@ -104,6 +110,7 @@ def makeArm(enemy):
     arm.speed = random.randint(5,10)
     arms.append(arm)
     actors.append(arm)
+
 
 def moveSheep(sheepSpeed):
     global gameState
@@ -123,8 +130,9 @@ def moveSheep(sheepSpeed):
         updateState('defend')
         clock.schedule_unique(mainState, 2)
 
+
 def playerMovement():
-    if ammon.isDead: return
+    if gameState == 'lose': return
     if (keyboard.left or keyboard.a) and ammon.left > 0 and not ammon.isAttacking:
         ammon.x -= ammonSpeed
         ammon.dir = "left"
@@ -136,6 +144,7 @@ def playerMovement():
     if (keyboard.down or keyboard.s) and ammon.bottom < HEIGHT and not ammon.isAttacking:
         ammon.y += ammonSpeed
     ammon.isMoving = keyboard.left or keyboard.right or keyboard.up or keyboard.down or keyboard.a or keyboard.d or keyboard.w or keyboard.s
+
 
 def updateSprite(spr):
     if spr.isDead:
@@ -163,15 +172,24 @@ def updateSprite(spr):
             spr.isAttacking = False
             spr.attacking = 0
 
+
 def on_key_down(key):
     if key == keys.SPACE:
-        ammon.isAttacking = True
+        if gameState == 'again':
+            reset()
+            updateState("sheep-off")
+        elif gameState == 'start':
+            updateState('sheep-off')
+        else:
+            ammon.isAttacking = True
+
 
 def checkattack(attacker, victim):
     r = attacker.dir == "right" and attacker.x < victim.x < attacker.right-attackAdj
     l = attacker.dir == "left" and attacker.left+attackAdj < victim.x < attacker.x
     y = abs(attacker.y - victim.y) < 80
     return y and (r or l)
+
 
 def sort(l):
     if len(l) < 2:
@@ -190,23 +208,78 @@ def sort(l):
             greater.append(x)
     return sort(less) + equal + sort(greater)
 
+
 def sortActors():
     global actors
     actors = sort(actors)
 
+
 def updateState(newState):
     global gameState
     gameState = newState
-    print(newState)
+
 
 def mainState():
     updateState('main')
 
+
+def againState():
+    updateState('again')
+
+
+def reset():
+    global actors, ammon, arms, enemies, sheep, enemiesKilled, sheepDir, lastSheep
+    actors = []
+
+    ammon = Actor(getImg("right", 24, standingOffset, 0), (WIDTH * 1.5, HEIGHT/2))
+    ammon.offset = 24
+    ammon.isMoving = True
+    ammon.walking = 0
+    ammon.dir = "left"
+    ammon.isAttacking = False
+    ammon.attacking = 0
+    ammon.isDead = False
+    ammon.dying = 0
+    ammon.dyingNum = 5
+    actors.append(ammon)
+
+    arms = []
+    enemies = []
+    enemiesKilled = 0
+
+    sheepDir = "right"
+    lastSheep = None
+    sheep = []
+    for i in range(sheepCount):
+        s = Actor(folder + sheepDir + "/sheep1", (random.randint(0, WIDTH/2), random.randint(0, HEIGHT)))
+        s.costume = random.randint(0,7)
+        s.dir = -1 if random.randint(0,1) == 0 else 1
+        sheep.append(s)
+        actors.append(s)
+        if lastSheep == None or s.x < lastSheep.x:
+            lastSheep = s
+
+    for i in range(enemyCount):
+        enemy = Actor(getImg("left", 0, standingOffset, 0))
+        enemy.x = (WIDTH/2) + ((500*(i+1)) * (-1 if random.randint(0,1) == 1 else 1))
+        enemy.y = (HEIGHT/2) + ((500*(i+1)) * (-1 if random.randint(0,1) == 1 else 1))
+        enemy.offset = 0
+        enemy.isMoving = False
+        enemy.walking = 0
+        enemy.dir = "left" if enemy.x > ammon.x else "right"
+        enemy.isAttacking = False
+        enemy.attacking = 0
+        enemy.isDead = False
+        enemy.dying = 0
+        enemy.dyingNum = 6
+        enemies.append(enemy)
+        actors.append(enemy)
+    
+    
 folder = "bom_game/"
 img = "/tile0"
 
-updateState("sheep-off")
-
+# frame vars
 walkingOffset = 0
 walkingNum = 6
 standingOffset = 6
@@ -221,56 +294,12 @@ attackAdj = 40
 grass = Actor("bom_game/grass")
 sRect = ZRect(0,0,WIDTH,HEIGHT)
 
-actors = []
-
-ammon = Actor(getImg("right", 24, standingOffset, 0), (WIDTH * 1.5, HEIGHT/2))
-ammon.offset = 24
-ammon.isMoving = True
-ammon.walking = 0
-ammon.dir = "left"
-ammon.isAttacking = False
-ammon.attacking = 0
-ammon.isDead = False
-ammon.dying = 0
-ammon.dyingNum = 5
 ammonSpeed = 5
-actors.append(ammon)
-
-arms = []
-enemies = []
-enemyCount = 1
-enemiesKilled = 0
+enemyCount = 2
 enemySpeed = 3
 enemyDeadSpeed = 5
-
-timer = 0
 sheepCount = 10
-sheepDir = "right"
-lastSheep = None
-sheep = []
-for i in range(sheepCount):
-    s = Actor(folder + sheepDir + "/sheep1", (random.randint(0, WIDTH/2), random.randint(0, HEIGHT)))
-    s.costume = random.randint(0,7)
-    s.dir = -1 if random.randint(0,1) == 0 else 1
-    sheep.append(s)
-    actors.append(s)
-    if lastSheep == None or s.x < lastSheep.x:
-        lastSheep = s
 
-for i in range(enemyCount):
-    enemy = Actor(getImg("left", 0, standingOffset, 0))
-    enemy.x = (WIDTH/2) + ((500*(i+1)) * (-1 if random.randint(0,1) == 1 else 1))
-    enemy.y = (HEIGHT/2) + ((500*(i+1)) * (-1 if random.randint(0,1) == 1 else 1))
-    enemy.offset = 0
-    enemy.isMoving = False
-    enemy.walking = 0
-    enemy.dir = "left" if enemy.x > ammon.x else "right"
-    enemy.isAttacking = False
-    enemy.attacking = 0
-    enemy.isDead = False
-    enemy.dying = 0
-    enemy.dyingNum = 6
-    enemies.append(enemy)
-    actors.append(enemy)
-
+reset()
+updateState("start")
 pgzrun.go()
