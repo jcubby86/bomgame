9368e5d309caa218fd74ed9730b1fbc447db4a66
Jacob Bastian <jcubby86@hotmail.com>
2026-02-19 22:50:05 -0700

    Player movement done. Sheep movement WIP

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -2,9 +2,10 @@ import { Scene } from 'phaser';
 import { EventBus } from '../EventBus';
 
 export class Game extends Scene {
-  private ammon: Phaser.Physics.Arcade.Sprite;
+  private player: Phaser.Physics.Arcade.Sprite;
   private sheep: Phaser.Physics.Arcade.Sprite[];
   private bandits: Phaser.Physics.Arcade.Sprite[];
+  private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
   constructor() {
     super('Game');
@@ -35,12 +36,12 @@ export class Game extends Scene {
     this.load.image('arm', 'arm.png');
   }
 
-  createAmmon() {
-    this.ammon = this.physics.add.sprite(512, 384, 'ammon', 6);
-    this.ammon.setOrigin(0, 0);
-    this.ammon.setCollideWorldBounds(true);
+  createPlayer() {
+    this.player = this.physics.add.sprite(512, 384, 'ammon', 6);
+    this.player.setOrigin(0.5, 0.5);
+    this.player.setCollideWorldBounds(true);
 
-    this.ammon.anims.create({
+    this.player.anims.create({
       key: 'walk',
       frames: this.anims.generateFrameNumbers('ammon', {
         start: 0,
@@ -49,7 +50,7 @@ export class Game extends Scene {
       frameRate: 10,
       repeat: -1
     });
-    this.ammon.anims.create({
+    this.player.anims.create({
       key: 'attack',
       frames: this.anims.generateFrameNumbers('ammon', {
         start: 6,
@@ -58,7 +59,7 @@ export class Game extends Scene {
       }),
       frameRate: 10
     });
-    this.ammon.anims.create({
+    this.player.anims.create({
       key: 'die',
       frames: this.anims.generateFrameNumbers('ammon', {
         start: 10,
@@ -67,27 +68,44 @@ export class Game extends Scene {
       frameRate: 10
     });
 
-    const spaceBar = this.input.keyboard?.addKey(
-      Phaser.Input.Keyboard.KeyCodes.SPACE
-    );
+    this.cursors?.space?.on('down', () => {
+      this.player.play('attack');
+    });
 
-    spaceBar?.on('down', () => {
-      console.log("Spacebar pressed");
-      this.ammon.play('attack');
+    this.player.on('animationcomplete-attack', () => {
+      this.player.chain('walk');
     });
   }
 
-  create() {
-    this.add.image(0, 0, 'background').setOrigin(0, 0);
-    this.createAmmon();
-    
+  createSheep() {
     this.anims.create({
-      key: 'sheep-walk',
+      key: 'sheep-run',
       frames: this.anims.generateFrameNumbers('sheep', { start: 0, end: 1 }),
       frameRate: 10,
       repeat: -1
     });
+    this.anims.create({
+      key: 'sheep-walk',
+      frames: this.anims.generateFrameNumbers('sheep', { start: 0, end: 1 }),
+      frameRate: 20,
+      repeat: -1
+    });
 
+    this.sheep = [];
+    for (let i = 0; i < 5; i++) {
+      const sheep = this.physics.add.sprite(
+        Phaser.Math.Between(100, 900),
+        Phaser.Math.Between(100, 700),
+        'sheep'
+      );
+      sheep.play('sheep-run');
+      sheep.setVelocityX(200);
+      sheep.setVelocityY(Phaser.Math.Between(-50, 50));
+      this.sheep.push(sheep);
+    }
+  }
+
+  createBandits() {
     this.anims.create({
       key: 'bandit-walk',
       frames: this.anims.generateFrameNumbers('bandit', { start: 0, end: 5 }),
@@ -110,46 +128,82 @@ export class Game extends Scene {
       repeat: -1
     });
 
+    this.bandits = [];
+    for (let i = 0; i < 3; i++) {
+      const bandit = this.physics.add.sprite(
+        Phaser.Math.Between(100, 900),
+        Phaser.Math.Between(100, 700),
+        'bandit'
+      );
+      bandit.play('bandit-walk');
+      this.bandits.push(bandit);
+    }
+  }
+
+  create() {
+    this.cursors = this.input.keyboard?.createCursorKeys();
+    this.add.image(0, 0, 'background').setOrigin(0, 0);
+    this.createPlayer();
+    this.createSheep();
+    this.createBandits();
+
     EventBus.emit('current-scene-ready', this);
   }
 
-  update() {
-    const cursors = this.input.keyboard?.createCursorKeys();
+  updatePlayer() {
+    const speed = 200;
 
-    cursors?.left.on('down', () => {
-      this.ammon.play('walk', true);
-      
+    this.player.setVelocity(0);
+
+    if (this.player.anims.currentAnim?.key === 'attack') {
+      return;
+    }
+
+    const offsetX = 10;
+    if (this.cursors?.left.isDown) {
+      this.player.setVelocityX(-speed);
+      this.player.setFlipX(true);
+      this.player.setDisplayOrigin(80 + offsetX, 80);
+    } else if (this.cursors?.right.isDown) {
+      this.player.setVelocityX(speed);
+      this.player.setFlipX(false);
+      this.player.setDisplayOrigin(80 - offsetX, 80);
+    }
+
+    if (this.cursors?.up.isDown) {
+      this.player.setVelocityY(-speed);
+    } else if (this.cursors?.down.isDown) {
+      this.player.setVelocityY(speed);
+    }
+
+    this.player.body?.velocity.normalize().scale(speed);
+
+    const velocityX = this.player.body?.velocity.x || 0;
+    const velocityY = this.player.body?.velocity.y || 0;
+
+    if (velocityX !== 0 || velocityY !== 0) {
+      this.player.play('walk', true);
+    } else if (this.player.anims.currentAnim?.key === 'walk') {
+      this.player.stop();
+      this.player.setFrame(6);
+    }
+  }
+
+  updateSheep() {
+    this.sheep.forEach((sheep) => {
+      if (Phaser.Math.Between(0, 100) < 1) {
+        sheep.setVelocityY(-1 * sheep.body!.velocity.y);
+      }
     });
+  }
 
-    let movingX = false, movingY = false;
+  update() {
+    this.updatePlayer();
+    this.updateSheep();
 
-    if (cursors?.left.isDown) {
-      this.ammon.setVelocityX(-200);
-      this.ammon.flipX = true;
-      movingX = true;
-    } else if (cursors?.right.isDown) {
-      this.ammon.setVelocityX(200);
-      this.ammon.flipX = false;
-      movingX = true;
-    }
-    if (cursors?.up.isDown) {
-      this.ammon.setVelocityY(-200);
-      movingY = true;
-    } else if (cursors?.down.isDown) {
-      this.ammon.setVelocityY(200);
-      movingY = true;
-    }
-
-    if (!movingX && !movingY) {
-      this.ammon.setVelocity(0, 0);
-      this.ammon.stop();
-      this.ammon.setFrame(6);
-    } else if (!movingX) {
-      this.ammon.setVelocityX(0);
-      this.ammon.play('walk', true);
-    } else if (!movingY) {
-      this.ammon.setVelocityY(0);
-      this.ammon.play('walk', true);
-    }
+    // Sort sprites by Y position for depth
+    this.player.setDepth(this.player.y);
+    this.sheep.forEach((sheep) => sheep.setDepth(sheep.y));
+    this.bandits.forEach((bandit) => bandit.setDepth(bandit.y));
   }
 }
